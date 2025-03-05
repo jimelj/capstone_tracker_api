d116f0a1bc358ee1375adb1ba6fa22823641f572
Jimel Joseph <3507684+jimelj@users.noreply.github.com>
2025-03-05 12:58:08 -0500

    database broke fixing

--- database.py
+++ database.py
@@ -429,7 +429,7 @@ async def update_parcels(updated_parcels):
                         skipped += 1
                 else:
                     await conn.execute("""
-                        INSERT INTO parcels (id, barcode, scan_status, last_scanned_when, address_name, address1, address2, city, state, zip, pod, inserted_at) 
+                        INSERT INTO parcels (id, barcode, scan_status, last_scanned_when, destination_name, address_name, address1, address2, city, state, zip, pod, inserted_at) 
                         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, NOW())
                     """, parcel_id, barcode, new_status, new_timestamp, destination_name, parcel["address"]["name"], parcel["address"]["address1"],
                        parcel["address"]["address2"], parcel["address"]["city"], parcel["address"]["state"], parcel["address"]["zip"], parcel.get("pod"))
